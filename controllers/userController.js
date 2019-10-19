619d3310f23324e039d000dbfb6a3ca471b919b8
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2019-10-19 17:58:10 +0200

    reinstall deps

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+const { OAuth2Client } = require('google-auth-library');
+const User = require('../models/User');
+
+const client = new OAuth2Client(process.env.OAUTH_CLIENT_ID);
+
+const verifyAuthToken = async token => {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.OAUTH_CLIENT_ID,
+    });
+    return ticket.getPayload();
+  } catch (err) {
+    console.error('Error verifying auth token', err);
+  }
+};
+
+const checkIfUserExists = async email => await User.findOne({ email }).exec();
+
+const createNewUser = googleUser => {
+  const { name, email, picture } = googleUser;
+  const user = { name, email, picture };
+  return new User(user).save();
+};
+
+exports.findOrCreateUser = async token => {
+  // verify auth token
+  const googleUser = await verifyAuthToken(token);
+  // check if the user exists
+  const user = await checkIfUserExists(googleUser.email);
+  // if user exists, return them; otherwise, create new user in db
+  return user || createNewUser(googleUser);
+};
---
+++ server.js
@@ -0,0 +1,41 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+const { findOrCreateUser } = require('./controllers/userController');
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  // eslint-disable-next-line no-console
+  .then(() => console.log('DB connected!'))
+  // eslint-disable-next-line no-console
+  .catch(err => console.error(err));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+    try {
+      if (req && req.headers && req.headers.authorization) {
+        authToken = req.headers.authorization;
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (err) {
+      console.error(`Unable to authenticate user with token ${authToken}`);
+      console.error(err);
+    }
+    return { currentUser };
+  },
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`Server listening on ${url}`);
+});
+
+/*
+Google, mongo, mapbox, cloudary
+*/
