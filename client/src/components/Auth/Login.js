2b9f3a127ea3d0e26fb3a56759536e36aa287345
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2019-10-20 15:43:10 +0200

    create pin and upload to cloudinary

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,28 +1,33 @@
 import React, { useContext } from 'react';
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import { GoogleLogin } from 'react-google-login';
-import { GraphQLClient } from 'graphql-request';
-import { ME_QUERY } from '../../graphql/queries';
+
 import Context from '../../context';
+import { ME_QUERY } from '../../graphql/queries';
+import { BASE_URL } from '../../client';
 
 const Login = ({ classes }) => {
   const { dispatch } = useContext(Context);
-  const onFailure = error => {
-    console.error(error);
-  };
+
   const onSuccess = async googleUser => {
     try {
       const idToken = googleUser.getAuthResponse().id_token;
-      const client = new GraphQLClient('http://localhost:4000/graphql', {
+      const client = new GraphQLClient(BASE_URL, {
         headers: { authorization: idToken },
       });
       const { me } = await client.request(ME_QUERY);
       dispatch({ type: 'LOGIN_USER', payload: me });
       dispatch({ type: 'IS_LOGGED_IN', payload: googleUser.isSignedIn() });
-    } catch (error) {
-      onFailure(error);
+    } catch (err) {
+      onFailure(err);
     }
+  };
+
+  const onFailure = err => {
+    console.error('Error logging in', err);
+    dispatch({ type: 'IS_LOGGED_IN', payload: false });
   };
 
   return (
