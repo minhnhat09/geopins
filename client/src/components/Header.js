6a5d94808334634945a0c5d2e09df3294cc963af
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2019-10-20 11:26:58 +0200

    add redirect if logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 // import AppBar from "@material-ui/core/AppBar";
 // import Toolbar from "@material-ui/core/Toolbar";
 // import Map from "@material-ui/icons/Map";
@@ -11,26 +11,26 @@ const Header = ({ classes }) => {
 
 const styles = theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   grow: {
     flexGrow: 1,
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center',
   },
   icon: {
     marginRight: theme.spacing.unit,
-    color: "green",
-    fontSize: 45
+    color: 'green',
+    fontSize: 45,
   },
   mobile: {
-    display: "none"
+    display: 'none',
   },
   picture: {
-    height: "50px",
-    borderRadius: "90%",
-    marginRight: theme.spacing.unit * 2
-  }
+    height: '50px',
+    borderRadius: '90%',
+    marginRight: theme.spacing.unit * 2,
+  },
 });
 
 export default withStyles(styles)(Header);
--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import * as serviceWorker from './serviceWorker';
 import Context from './context';
 import reducer from './reducer';
+import ProtectedRoute from './ProtectedRoute';
 
 const Root = () => {
   const initialState = useContext(Context);
@@ -17,7 +18,7 @@ const Root = () => {
     <Router>
       <Context.Provider value={{ state, dispatch }}>
         <Switch>
-          <Route exact path="/" component={App} />
+          <ProtectedRoute exact path="/" component={App} />
           <Route path="/login" component={Splash} />
         </Switch>
       </Context.Provider>
