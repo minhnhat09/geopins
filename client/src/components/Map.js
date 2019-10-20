dc60c648f8635aad5c9acae27e39f0f78ed29b3c
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2019-10-20 11:44:13 +0200

    implement header and signout

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,30 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import ReactMapGL from 'react-map-gl';
 
+const INITIAL_VIEWPORT = {
+  latitude: 48.81333923736466,
+  longitude: 2.2793228312542775,
+  zoom: 13,
+};
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoibWluaG5oYXQwOSIsImEiOiJjazFlcW83bG0wazY4M2psbTVoMGkxNHJrIn0.ct4HmPBekg3vEk737vffLw"
+        onViewportChange={newViewport => setViewport(newViewport)}
+        {...viewport}
+      />
+    </div>
+  );
 };
 
 const styles = {
